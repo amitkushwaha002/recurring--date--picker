fc88d8e1ebf773d9f30405003cb2b7320e599f55
amitkushwaha002 <96170636+amitkushwaha002@users.noreply.github.com>
2025-07-20 19:53:00 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,11 @@
 // Demo page rendering <RecurringDatePicker />
+import React from 'react';
+import RecurringDatePicker from '../components/RecurringDatePicker';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 flex items-center justify-center">
+      <RecurringDatePicker />
+    </main>
+  );
+}
